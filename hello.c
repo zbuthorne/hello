e9db86c4a25a55cc35b9125616f32c69271626b8
zbuthorne <zbuthorne@huskers.unl.edu>
2025-08-27 08:19:21 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Zoey Buthorne
+ * Date: 2025/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Zoey Buthorne\n");
+
+  return 0;
+}
